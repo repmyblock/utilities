b279537d8cd01ccf512d65dabb37ded6a6ca5012
Theo Chino <github@theochino.com>
2019-12-26 08:22:50 +0000

    Removed all the unecessary loading of the cache to delete the data

--- VoterLists/Normalize/EmptyDB.pl
+++ VoterLists/Normalize/EmptyDB.pl
@@ -3,19 +3,14 @@
 ### Need to document what this file is about.
 use strict;
 use DBI;
-use Text::CSV;
 
 use FindBin::libs;
 use RepMyBlock;
-use RepMyBlock::NYS;
 
 print "Start the Empty Database program\n";
 
 my $dbh 				= RepMyBlock::InitDatabase();
-my $DateTable 	= RepMyBlock::InitTheVoter();
-my $DateTableID = RepMyBlock::DateDBID();
 
-RepMyBlock::InitCaches();
 RepMyBlock::EmptyDatabases("VotersLastName", 1);
 RepMyBlock::EmptyDatabases("VotersFirstName", 1);
 RepMyBlock::EmptyDatabases("VotersMiddleName", 1);
